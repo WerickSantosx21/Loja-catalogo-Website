37fb009fcc1bd3c603b4d4d647516197de2fd3e4
Werick Santos <149383332+WerickSantosx21@users.noreply.github.com>
2026-03-21 12:25:23 -0300

    facilitação de rotas com python + estilização e adicão na pagina Races

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, abort, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,10 +6,28 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/<page_name>')
+def render_static(page_name):
+    # essas são as páginas permitidas para serem renderizadas, metodo de segurança 
+    allowed_pages = ['home', 'Races', 'Contact', 'About']
 
-@app.route('/Races')
-def races():
-    return render_template('Races.html')
+   
+    if page_name in allowed_pages:
+        return render_template(f'{page_name}.html')
+    
+    abort(404)
+    # Se o caba tentar accesar outra pagina que não esteja na lista, ele vai receber um erro 404
+
+    
+#aqui cria uma função para lidar com erros 404.
+@app.errorhandler(404)
+def page_not_found(e):
+    # Se você tiver um arquivo 404.html, ele será usado aqui
+    # Se não tiver, o Flask usa o padrão dele para erros 404
+    try:
+        return render_template('404.html'), 404
+    except:
+        return "404 - Página Não Encontrada", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
